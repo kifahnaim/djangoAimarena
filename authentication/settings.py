fe4a22b959418202b3d7d4999f4163443764ed05
Kifah <naimkifah@gmail.com>
2021-05-14 22:47:33 +0300

    tweaked app for heroku

--- authentication/settings.py
+++ authentication/settings.py
@@ -169,3 +169,7 @@ CKEDITOR_CONFIGS = {
 }
 
 django_heroku.settings(locals())
+
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
